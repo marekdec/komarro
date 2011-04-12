f8703a0fdfef7ca060aadc616895e09ace70cbf5
marekdec@gmail.com <marekdec@gmail.com@a7e7578f-4e98-ae9e-ccbf-923176c622d4>
2011-04-12 07:54:33 +0000

    MockitoMockRecorder completed

--- mockarro-core/src/main/java/com/googlecode/mockarro/MockitoMockRecorder.java
+++ mockarro-core/src/main/java/com/googlecode/mockarro/MockitoMockRecorder.java
@@ -1,21 +1,25 @@
 package com.googlecode.mockarro;
 
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyByte;
+import static org.mockito.Matchers.anyChar;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Matchers.anyFloat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyShort;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import org.mockito.Matchers;
-
 public class MockitoMockRecorder {
-
-    private static Object any = Matchers.any();
 
 
     public void record(final Object mock, final Method method, final Object value) {
-        final int paramsCount = method.getParameterTypes().length;
         try {
-            when(method.invoke(mock, createParams(paramsCount))).thenReturn(value);
+            when(method.invoke(mock, createParams(method.getParameterTypes()))).thenReturn(value);
         } catch (final IllegalAccessException e) {
             e.printStackTrace();
         } catch (final InvocationTargetException e) {
@@ -24,11 +28,31 @@ public class MockitoMockRecorder {
     }
 
 
-    private Object[] createParams(final int count) {
-        final Object[] params = new Object[count];
-        for (int i = 0; i < count; i++) {
-            params[i] = any;
+    private Object[] createParams(final Class<?>[] paramTypes) {
+        final Object[] params = new Object[paramTypes.length];
+
+        for (int i = 0; i < paramTypes.length; i++) {
+            if (paramTypes[i].equals(int.class)) {
+                params[i] = anyInt();
+            } else if (paramTypes[i].equals(float.class)) {
+                params[i] = anyFloat();
+            } else if (paramTypes[i].equals(double.class)) {
+                params[i] = anyDouble();
+            } else if (paramTypes[i].equals(boolean.class)) {
+                params[i] = anyBoolean();
+            } else if (paramTypes[i].equals(char.class)) {
+                params[i] = anyChar();
+            } else if (paramTypes[i].equals(byte.class)) {
+                params[i] = anyByte();
+            } else if (paramTypes[i].equals(short.class)) {
+                params[i] = anyShort();
+            } else if (paramTypes[i].equals(long.class)) {
+                params[i] = anyLong();
+            } else {
+                params[i] = anyObject();
+            }
         }
         return params;
     }
+
 }
