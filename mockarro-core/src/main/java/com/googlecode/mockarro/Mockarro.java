fecb5da560c29f6897f6d6196cbd69c81c2159d0
marekdec@gmail.com <marekdec@gmail.com@a7e7578f-4e98-ae9e-ccbf-923176c622d4>
2011-04-28 15:42:11 +0000

    Simplified mockarro dsl

--- mockarro-core/src/main/java/com/googlecode/mockarro/Mockarro.java
+++ mockarro-core/src/main/java/com/googlecode/mockarro/Mockarro.java
@@ -4,6 +4,7 @@ import static com.googlecode.mockarro.MethodSieve.methodsOf;
 import static com.googlecode.mockarro.injector.Injector.withMockEngine;
 
 import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
@@ -11,60 +12,91 @@ import java.util.WeakHashMap;
 import com.googlecode.mockarro.injector.InjectionEngine.Injection;
 import com.googlecode.mockarro.injector.MockitoMockEngine;
 
-public class Mockarro {
+public class Mockarro<T> {
 
-    private static Map<Object, Set<Injection>> mocksBySut = new WeakHashMap<Object, Set<Injection>>();
+    private static Map<Thread, Set<Injection>> mocksByThread = Collections
+                                                                     .synchronizedMap(new WeakHashMap<Thread, Set<Injection>>());
 
-    private final Set<Injection>               mockDescription;
+    private final Set<Injection>               mocks;
+
+    private final Class<?>                     returnType;
+    private final MockitoMockRecorder          recorder      = new MockitoMockRecorder();
 
 
+    /**
+     * Initialises Mockarro for given system under test. Its basic task is to
+     * reset the test by creating and injecting mocks into the system under
+     * test.
+     * <p>
+     * This method has to be invoked before every single test execution -
+     * preferably within a before method (a method annotated with @Before in
+     * JUnit 4.x or @BeforeMethod in TestNG).
+     * 
+     * @param systemUnderTest
+     *            the unit that is going to be tested.
+     */
     public static void initSut(final Object systemUnderTest) {
         final Set<Injection> injections = withMockEngine(new MockitoMockEngine()).createInjector().andInject(
                 systemUnderTest);
-        mocksBySut.put(systemUnderTest, injections);
-    }
-
-
-    private Mockarro(final Set<Injection> mocks) {
-        super();
-        this.mockDescription = mocks;
-    }
-
-
-    public static Mockarro whenever(final Object systemUnderTest) {
-        return new Mockarro(mocksBySut.get(systemUnderTest));
+        mocksByThread.put(Thread.currentThread(), injections);
     }
 
 
 
-    public <T> BehaviorRegistration<T> requests(final Class<T> typeOfRequestedValue) {
-        return new BehaviorRegistration<T>(mockDescription, typeOfRequestedValue);
-    }
 
-    public static class BehaviorRegistration<T> {
-        private final Set<Injection>      mocks;
-
-        private final Class<?>            returnType;
-        private final MockitoMockRecorder recorder = new MockitoMockRecorder();
-
-
-        private BehaviorRegistration(final Set<Injection> mocks, final Class<?> returnType) {
-            super();
-            this.mocks = mocks;
-            this.returnType = returnType;
+    /**
+     * Registers behaviour for all of the methods that return an object or a
+     * primitive of a declared type.
+     * <p>
+     * Throws an {@link IllegalStateException} if the {@link #initSut(Object)}
+     * method has not been invoked for the current system under test before
+     * invoking this method.
+     * 
+     * @param <T>
+     *            will be automatically inferred.
+     * @param typeOfRequestedValue
+     *            The type literal i.e. class or primitive literal. For example
+     *            if the requested type is of MyClass type the
+     *            typeOfRequestedValue will be MyClass.class, if the requested
+     *            object is of a primitive integer type use int.class, etc.
+     * @return a Mockarro object that can be used for ongoing stubbing.
+     */
+    public static <T> Mockarro<T> whenRequested(final Class<T> typeOfRequestedValue) {
+        final Set<Injection> mockDescription = mocksByThread.get(Thread.currentThread());
+        if (mockDescription == null) {
+            throw new IllegalStateException(
+                    "The mockarro test has not been initialized yet.\nAre you sure you have called the init method with the current unit under test as the parameter?");
         }
+        return new Mockarro<T>(mockDescription, typeOfRequestedValue);
+    }
 
 
-        public void thenReturn(final T recordedValue) {
-            for (final Injection mockDescription : mocks) {
 
-                for (final Method method : methodsOf(mockDescription.actualClass()).thatReturn(returnType).asSet()) {
-                    if (!method.getName().equals("hashCode") && !method.getName().equals("equals")) {
-                        recorder.record(mockDescription.getMock(), method, recordedValue);
-                    }
+
+    private Mockarro(final Set<Injection> mocks, final Class<?> returnType) {
+        super();
+        this.mocks = mocks;
+        this.returnType = returnType;
+    }
+
+
+    /**
+     * Defines the value that will be returned when object of defined type is
+     * requested.
+     * 
+     * @param recordedValue
+     *            the vale that will be returned by the mocked object when the
+     *            mock object is requested to return the value of previously
+     *            defined type.
+     */
+    public void thenReturn(final T recordedValue) {
+        for (final Injection mockDescription : mocks) {
+
+            for (final Method method : methodsOf(mockDescription.actualClass()).thatReturn(returnType).asSet()) {
+                if (!method.getName().equals("hashCode") && !method.getName().equals("equals")) {
+                    recorder.record(mockDescription.getMock(), method, recordedValue);
                 }
             }
         }
     }
-
 }
